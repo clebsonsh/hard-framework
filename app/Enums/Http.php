54b51a3d011849c1304b2adefcdbcd980d2a556a
Clebson Moura <clebson.sh@gmail.com>
2025-08-17 20:53:03 -0300

    feat: add enum for http verbs

---
+++ app/Enums/Http.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum Http: string
+{
+    case GET = 'get';
+    case POST = 'post';
+    case PUT = 'put';
+    case PATCH = 'patch';
+    case DELETE = 'delete';
+}
