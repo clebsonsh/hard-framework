2122c3a45b9c5d51c315aa35107d5c522aacda64
Clebson Moura <clebson.sh@gmail.com>
2025-08-21 15:05:43 -0300

    test: add tests for createFromGlobals method

--- tests/Unit/Http/RequestTest.php
+++ tests/Unit/Http/RequestTest.php
@@ -4,6 +4,23 @@ use Infra\Enums\HttpMethod;
 use Infra\Http\Request;
 
 describe('request', function () {
+    it('should create a request from globals', function () {
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['REQUEST_URI'] = '/test';
+
+        $request = Request::createFromGlobals();
+
+        expect($request)->toBeInstanceOf(Request::class);
+    });
+
+    it('should not create a request from globals if method is null', function () {
+        $_SERVER['REQUEST_METHOD'] = null;
+        $_SERVER['REQUEST_URI'] = '/test';
+
+        expect(fn () => Request::createFromGlobals())
+            ->toThrow(RuntimeException::class);
+    });
+
     it('should be created', function () {
         $path = '/test';
         $method = HttpMethod::GET;
