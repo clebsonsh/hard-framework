939e8e5454dcd1ccd250a2d72c909eeff6cdea92
Clebson Moura <clebson.sh@gmail.com>
2025-08-21 12:58:22 -0300

    test: add unit test for Request

--- tests/Unit/Http/RequestTest.php
+++ tests/Unit/Http/RequestTest.php
@@ -1,1 +1,18 @@
 <?php
+
+use Infra\Enums\HttpMethod;
+use Infra\Http\Request;
+
+describe('request', function () {
+    it('should be created', function () {
+        $path = '/test';
+        $method = HttpMethod::GET;
+        $data = [
+            'test' => 'data',
+        ];
+
+        $request = new Request($path, $method, $data);
+
+        expect($request)->toBeInstanceOf(Request::class);
+    });
+});
