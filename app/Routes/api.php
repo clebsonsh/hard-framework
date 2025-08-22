7a1cfec46d6f74f80687045f09fdab04bb80ebb2
Clebson Moura <clebson.sh@gmail.com>
2025-08-22 11:13:00 -0300

    chore: remove duplicated example code

--- app/Routes/api.php
+++ app/Routes/api.php
@@ -3,10 +3,8 @@
 declare(strict_types=1);
 
 use App\Handlers\Api\PostHandler;
-use App\Handlers\Api\TestHandler;
 use Infra\Http\Router;
 
 return function (Router $router) {
-    $router->get('/api/test', new TestHandler);
     $router->post('/api/post', new PostHandler);
 };
