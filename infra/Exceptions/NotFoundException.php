f70a5fbc88d608d7562fcd80ee4b5f5637a8937a
Clebson Moura <clebson.sh@gmail.com>
2025-08-23 00:58:21 -0300

    refactor: remove unused constructor

--- infra/Exceptions/NotFoundException.php
+++ infra/Exceptions/NotFoundException.php
@@ -5,12 +5,5 @@ declare(strict_types=1);
 namespace Infra\Exceptions;
 
 use Exception;
-use Throwable;
 
-class NotFoundException extends Exception
-{
-    public function __construct(string $message = 'Route Not Found', int $code = 0, ?Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-}
+class NotFoundException extends Exception {}
