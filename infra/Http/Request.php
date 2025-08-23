1bc175017780e6382aa5ce8c2233090c115bd268
Clebson Moura <clebson.sh@gmail.com>
2025-08-23 20:24:38 -0300

    feature: parse headers into Request, differentiate html requests from json requests

--- infra/Http/Request.php
+++ infra/Http/Request.php
@@ -12,27 +12,45 @@ class Request
     /** @var array<string,float|int|string> */
     private array $params;
 
+    /** @var string[] */
+    private readonly array $headers;
+
     /**
      * @param  string[]  $data
+     * @param  string[]  $headers
      */
     public function __construct(
         private readonly string $path,
         private readonly HttpMethod $httpMethod,
         private readonly array $data,
+        array $headers = []
     ) {
         $this->params = [];
+        $this->headers = $headers;
     }
 
     public static function createFromGlobals(): self
     {
         return new self(
-            path: self::getUrlPath(),
-            httpMethod: self::getHttpMethod(),
-            data: self::getData(),
+            path: self::getPathFormGlobals(),
+            httpMethod: self::getMethodFromGlobals(),
+            data: self::getDataFromGlobals(),
+            headers: self::getHeadersFromGlobals(),
         );
     }
 
-    private static function getHttpMethod(): HttpMethod
+    private static function getPathFormGlobals(): string
+    {
+        /** @var string $uri */
+        $uri = $_SERVER['REQUEST_URI'];
+
+        /** @var string $path */
+        $path = parse_url($uri, PHP_URL_PATH);
+
+        return $path;
+    }
+
+    private static function getMethodFromGlobals(): HttpMethod
     {
         /** @var ?string $requestMethod */
         $requestMethod = $_SERVER['REQUEST_METHOD'] ?? null;
@@ -44,10 +62,8 @@ class Request
         return HttpMethod::from(strtolower($requestMethod));
     }
 
-    /**
-     * @return string[]
-     */
-    private static function getData(): array
+    /** @return string[] */
+    private static function getDataFromGlobals(): array
     {
         /** @var string[] $getData */
         $getData = $_GET;
@@ -61,15 +77,22 @@ class Request
         return array_merge($getData, $postData, $jsonData);
     }
 
-    private static function getUrlPath(): string
+    /** @return string[] */
+    private static function getHeadersFromGlobals(): array
     {
-        /** @var string $uri */
-        $uri = $_SERVER['REQUEST_URI'];
+        /** @var string[] $headers */
+        $headers = [];
+        
+        foreach ($_SERVER as $name => $value) {
+            if (str_starts_with($name, 'HTTP_')) {
+                // Normalizes the header name from 'HTTP_CONTENT_TYPE' to 'content-type'.
+                $headerName = str_replace('_', '-', strtolower(substr($name, 5)));
 
-        /** @var string $path */
-        $path = parse_url($uri, PHP_URL_PATH);
+                $headers[$headerName] = $value;
+            }
+        }
 
-        return $path;
+        return $headers;
     }
 
     public function int(string $field): int
@@ -103,6 +126,12 @@ class Request
         return $this->httpMethod;
     }
 
+    /** @return string[] */
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
+
     /** @param array<string,float|int|string> $params */
     public function setParams(array $params): void
     {
@@ -118,5 +147,14 @@ class Request
     public function getParam(string $key, mixed $default = null): mixed
     {
         return $this->params[$key] ?? $default;
+    }
+
+    public function wantsJson(): bool
+    {
+        if (str_starts_with($this->getPath(), '/api/')) {
+            return true;
+        }
+
+        return str_contains($this->headers['accept'] ?? '', 'application/json');
     }
 }
