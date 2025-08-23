5175711d35634ebadc3757ff6541f82af13e9d63
Clebson Moura <clebson.sh@gmail.com>
2025-08-23 20:26:53 -0300

    test: add test to not found json requests

--- tests/Unit/infra/Http/Handlers/NotFoundHandlerTest.php
+++ tests/Unit/infra/Http/Handlers/NotFoundHandlerTest.php
@@ -10,8 +10,19 @@ use Infra\Http\Response;
 use Infra\Http\Router;
 
 describe('Not Found', function () {
-    it('should return not found response', function () {
+    it('should return an html not found response for web requests', function () {
         $request = new Request('/not-found', HttpMethod::GET, []);
+        $router = new Router($request);
+
+        $response = $router->handleRequest();
+
+        expect($response)->toBeInstanceOf(Response::class)
+            ->and($response->getBody())->toBe('<h1>404 Not Found</h1>')
+            ->and($response->getStatus())->toBe(404);
+    });
+
+    it('should return a json not found response for api requests', function () {
+        $request = new Request('/api/not-found', HttpMethod::GET, []);
         $router = new Router($request);
 
         $response = $router->handleRequest();
