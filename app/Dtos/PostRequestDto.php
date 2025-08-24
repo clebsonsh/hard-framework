cc13e86e8f0631b87f34fef3df771229e465d3a4
Clebson Moura <clebson.sh@gmail.com>
2025-08-24 16:52:39 -0300

    feat: implements example usage of RequestDto

---
+++ app/Dtos/PostRequestDto.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Dtos;
+
+use Infra\Interfaces\RequestDtoInterface;
+
+class PostRequestDto implements RequestDtoInterface
+{
+    public function __construct(
+        public string $title,
+        public ?string $content = '',
+    ) {}
+
+    /**
+     * @param  string[]  $data
+     */
+    public static function fromRequestData(array $data): self
+    {
+        $title = $data['title'] ?? '';
+
+        $content = $data['content'] ?? '';
+
+        return new self($title, $content);
+    }
+}
--- app/Handlers/Api/PostHandler.php
+++ app/Handlers/Api/PostHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Handlers\Api;
 
+use App\Dtos\PostRequestDto;
 use Infra\Http\Request;
 use Infra\Http\Response;
 use Infra\Interfaces\RequestHandlerInterface;
@@ -13,7 +14,7 @@ class PostHandler implements RequestHandlerInterface
     public function handle(Request $request): Response
     {
         return Response::json([
-            'data' => $request->getData(),
+            'data' => $request->getData(PostRequestDto::class),
             'params' => $request->getParams(),
         ]);
     }
